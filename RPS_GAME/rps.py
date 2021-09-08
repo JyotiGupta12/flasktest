cc6d0f489687fe31adb12b0c46022f0d4d2d59c4
JyotiGupta12 <46385627+JyotiGupta12@users.noreply.github.com>
2021-09-07 17:08:36 -0700

    Add files via upload

---
+++ RPS_GAME/rps.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, url_for,flash,redirect
+# adding html template
+from flask import render_template
+import random
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/rpsGame', methods = ["GET","POST"])
+def rpsGame():
+    comp_list = ["paper", "rock", "scissor"]
+    comp_value = random.choice(comp_list)
+    if request.method == "POST":
+        user_item = request.form['item']
+        return render_template('rpsGameOutput.html',user_item = user_item,comp_value = comp_value)
+    return render_template('rpsGameSetup.html')
+
+if __name__ == '__main__':
+    # sess.init_app(app)
+    app.run(debug=True)
+    # to run on cmd
